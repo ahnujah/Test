8c1341106e6d259f04622a0cb005099b4a91668b
ahnujah <153641858+ahnujah@users.noreply.github.com>
2025-02-05 22:23:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,68 +1,79 @@
 #!/bin/bash
 
 # Czaractyl Installation Script
+# https://github.com/ahnujah/Test
 
+echo "==================================="
 echo "Welcome to Czaractyl Installation"
-echo "Server Type: $SERVER_TYPE"
-echo "Server Version: $SERVER_VERSION"
-echo "Build Number: $BUILD_NUMBER"
+echo "==================================="
+echo "Please choose your server software:"
+echo "1. Paper (Latest builds of Paper)"
+echo "2. Forge (Modded Minecraft)"
+echo "3. Fabric (Modern mod loader)"
+echo "4. Vanilla (Official Minecraft)"
+echo "5. Spigot (CraftBukkit fork)"
+echo "==================================="
 
-case $SERVER_TYPE in
-    paper)
-        if [ "$SERVER_VERSION" == "latest" ]; then
-            SERVER_VERSION=$(curl -s https://papermc.io/api/v2/projects/paper | jq -r '.versions[-1]')
-        fi
-        if [ "$BUILD_NUMBER" == "latest" ]; then
-            BUILD_NUMBER=$(curl -s https://papermc.io/api/v2/projects/paper/versions/${SERVER_VERSION} | jq -r '.builds[-1]')
-        fi
-        JAR_NAME="paper-${SERVER_VERSION}-${BUILD_NUMBER}.jar"
-        DOWNLOAD_URL="https://papermc.io/api/v2/projects/paper/versions/${SERVER_VERSION}/builds/${BUILD_NUMBER}/downloads/${JAR_NAME}"
+read -p "Enter your choice (1-5): " choice
+
+# Install required tools
+apk add --no-cache curl jq
+
+case $choice in
+    1)
+        echo "Installing Paper..."
+        LATEST_VERSION=$(curl -s https://papermc.io/api/v2/projects/paper | jq -r '.versions[-1]')
+        BUILD_NUMBER=$(curl -s https://papermc.io/api/v2/projects/paper/versions/${LATEST_VERSION} | jq -r '.builds[-1]')
+        JAR_NAME="paper-${LATEST_VERSION}-${BUILD_NUMBER}.jar"
+        DOWNLOAD_URL="https://papermc.io/api/v2/projects/paper/versions/${LATEST_VERSION}/builds/${BUILD_NUMBER}/downloads/${JAR_NAME}"
+        curl -o server.jar $DOWNLOAD_URL
         ;;
-    forge)
-        # You'll need to implement Forge version fetching logic here
-        DOWNLOAD_URL="https://files.minecraftforge.net/maven/net/minecraftforge/forge/${SERVER_VERSION}/forge-${SERVER_VERSION}-installer.jar"
-        JAR_NAME="forge-${SERVER_VERSION}-installer.jar"
+    2)
+        echo "Installing Forge..."
+        read -p "Enter Minecraft version (e.g., 1.19.2): " MC_VERSION
+        FORGE_VERSION=$(curl -s https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json | jq -r --arg MC_VERSION "$MC_VERSION" '.promos | to_entries[] | select(.key | startswith($MC_VERSION)) | .value' | head -n 1)
+        DOWNLOAD_URL="https://maven.minecraftforge.net/net/minecraftforge/forge/${MC_VERSION}-${FORGE_VERSION}/forge-${MC_VERSION}-${FORGE_VERSION}-installer.jar"
+        curl -o forge-installer.jar $DOWNLOAD_URL
+        java -jar forge-installer.jar --installServer
+        rm forge-installer.jar
+        mv forge-*-universal.jar server.jar
         ;;
-    fabric)
+    3)
+        echo "Installing Fabric..."
+        read -p "Enter Minecraft version (e.g., 1.19.2): " MC_VERSION
         FABRIC_VERSION=$(curl -s https://meta.fabricmc.net/v2/versions/installer | jq -r '.[0].version')
-        DOWNLOAD_URL="https://maven.fabricmc.net/net/fabricmc/fabric-installer/${FABRIC_VERSION}/fabric-installer-${FABRIC_VERSION}.jar"
-        JAR_NAME="fabric-installer.jar"
+        curl -o fabric-installer.jar https://maven.fabricmc.net/net/fabricmc/fabric-installer/${FABRIC_VERSION}/fabric-installer-${FABRIC_VERSION}.jar
+        java -jar fabric-installer.jar server -mcversion $MC_VERSION -downloadMinecraft
+        rm fabric-installer.jar
+        mv fabric-server-launch.jar server.jar
         ;;
-    sponge)
-        # You'll need to implement Sponge version fetching logic here
-        DOWNLOAD_URL="https://repo.spongepowered.org/maven/org/spongepowered/spongevanilla/${SERVER_VERSION}/spongevanilla-${SERVER_VERSION}.jar"
-        JAR_NAME="sponge-${SERVER_VERSION}.jar"
-        ;;
-    vanilla)
+    4)
+        echo "Installing Vanilla Minecraft..."
         MANIFEST_URL="https://launchermeta.mojang.com/mc/game/version_manifest.json"
-        if [ "$SERVER_VERSION" == "latest" ]; then
-            SERVER_VERSION=$(curl -s $MANIFEST_URL | jq -r '.latest.release')
-        fi
-        VERSION_URL=$(curl -s $MANIFEST_URL | jq -r --arg VERSION "$SERVER_VERSION" '.versions[] | select(.id == $VERSION) | .url')
+        LATEST_VERSION=$(curl -s $MANIFEST_URL | jq -r '.latest.release')
+        VERSION_URL=$(curl -s $MANIFEST_URL | jq -r --arg VERSION "$LATEST_VERSION" '.versions[] | select(.id == $VERSION) | .url')
         DOWNLOAD_URL=$(curl -s $VERSION_URL | jq -r '.downloads.server.url')
-        JAR_NAME="minecraft_server.${SERVER_VERSION}.jar"
+        curl -o server.jar $DOWNLOAD_URL
+        ;;
+    5)
+        echo "Installing Spigot..."
+        mkdir -p BuildTools && cd BuildTools
+        curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+        java -jar BuildTools.jar --rev latest
+        mv spigot-*.jar ../server.jar
+        cd .. && rm -rf BuildTools
         ;;
     *)
-        echo "Invalid server type specified"
+        echo "Invalid choice. Exiting."
         exit 1
         ;;
 esac
 
-echo "Downloading server jar..."
-curl -o server.jar $DOWNLOAD_URL
+# Create server.properties with basic configuration
+echo "server-port=${SERVER_PORT}" > server.properties
+echo "motd=A Minecraft Server powered by Czaractyl" >> server.properties
 
-if [ "$SERVER_TYPE" == "forge" ]; then
-    echo "Installing Forge server..."
-    java -jar server.jar --installServer
-    rm server.jar
-    mv forge-*-universal.jar server.jar
-elif [ "$SERVER_TYPE" == "fabric" ]; then
-    echo "Installing Fabric server..."
-    java -jar server.jar server -mcversion $SERVER_VERSION -downloadMinecraft
-    rm server.jar
-    mv fabric-server-launch.jar server.jar
-fi
-
+# Accept EULA
 echo "eula=true" > eula.txt
 
-echo "Installation complete!"
+echo "Installation complete! Server jar has been downloaded as server.jar"
